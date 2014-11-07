642a681c51b7e4aef7701e78948b12dc719ed3d2
piotrkrawcz <piotrkrawcz@o2.pl>
2014-11-07 13:10:41 +0100

    Delete ex7.py

--- ex7.py
+++
@@ -1,8 +0,0 @@
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weight?",
-weight = raw_input()
-
-print "So you are %r yeras old, you have %r cm height, and yout weight is %r" % (age, height, weight)
