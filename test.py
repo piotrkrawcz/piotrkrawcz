461f5b52509c47208227cb69158a69d95a7c05b1
piotrkrawcz <piotrkrawcz@o2.pl>
2014-11-26 11:12:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,36 @@
-lokacje = [
-		Wioska(),
-		Sklep(),
-		Las(),
-]
-przejscia = {
-  0: [1, 2],
-  1: [2],
-  3: [0, 1],
-}
+wersja1
+class Lokacja(object):
+	def __init__(self):
+		self.przypadek = dict(
+			mianownik = self.mianownik,
+			dopelniacz = self.dopelniacz,
+			narzednik = self.narzednik,
+		
+		)
+	def nazwa(self, odmiana):
+		return self.przypadek[odmiana]
+		
+class Wioska(Lokacja):
+	mianownik = 'wioska'
+	dopelniacz = 'wioski'
+	narzednik = 'wioska'
+	
+	def wejscie(self, gracz):
+		pass
+wersja 2		
+class Lokacja(object):
+	przypadek = dict(
+			mianownik = self.przypadki[0],
+			dopelniacz = self.przypadki[1],
+			narzednik = self.przypadki[2],
+		
+		)
+	def nazwa(self, odmiana):
+		return self.przypadek[odmiana]
+		
+class Wioska(Lokacja):
+	przypadki = ['wioska','wioski','wioska']
+	
+		
+	def wejscie(self, gracz):
+		pass
